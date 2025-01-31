47f4b43df33a2a1dc58c572ec7782fa18e848168
Abdulsalam <habeebabdulsalam86@gmail.com>
2025-01-31 13:19:10 +0100

    Adjusted the AddStaffModal

--- src/components/TSPRegistration/AddStaffModal.vue
+++ src/components/TSPRegistration/AddStaffModal.vue
@@ -1,66 +1,195 @@
-<!-- AddStaffModal.vue -->
 <template>
-  <div v-if="isOpen" class="modal-overlay">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">
-          {{ isEditing ? "Edit Staff" : "Add New Staff" }}
-        </h5>
-        <button type="button" class="btn-close" @click="closeModal"></button>
-      </div>
-      <div class="modal-body">
-        <form @submit.prevent="submitForm">
-          <div class="form-group">
-            <label>Name</label>
-            <input
-              type="text"
-              v-model="formData.name"
-              class="form-control"
-              required
-            />
-          </div>
-          <div class="form-group">
-            <label>Rank</label>
-            <select v-model="formData.rank" class="form-control" required>
-              <option value="" disabled selected>Select Rank</option>
-              <option v-for="rank in ranks" :key="rank" :value="rank">
-                {{ rank }}
-              </option>
-            </select>
-          </div>
-          <div class="form-group">
-            <label>Years of Experience</label>
-            <input
-              type="number"
-              v-model.number="formData.experience"
-              class="form-control"
-              min="0"
-              max="50"
-              placeholder="Enter years of experience"
-              required
-            />
-          </div>
-          <div class="form-group">
-            <label>Position</label>
-            <input
-              type="text"
-              v-model="formData.position"
-              class="form-control"
-              required
-            />
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" @click="closeModal">
-              Cancel
+  <!-- Modal -->
+  <div class="modal fade" :class="{ 'show d-block': isOpen }" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{ isEditing ? "Edit Staff" : "Add Staff details here" }}</h5>
+          <button type="button" class="btn-close" @click="closeModal"></button>
+        </div>
+
+        <div class="modal-body">
+          <form @submit.prevent="submitForm">
+            <div class="input-box mb-3">
+              <label class="label">Name</label>
+              <div class="input-wrapper">
+                <input 
+                  type="text" 
+                  class="form-control" 
+                  v-model="formData.name" 
+                  required
+                />
+              </div>
+              <span v-if="errors.name" class="error">{{ errors.name }}</span>
+            </div>
+
+            <div class="input-box mb-3">
+              <label class="label">Years of Experience</label>
+              <div class="input-wrapper">
+                <input 
+                  type="number" 
+                  class="form-control" 
+                  v-model.number="formData.experience" 
+                  min="0" 
+                  max="50" 
+                  required
+                />
+              </div>
+              <span v-if="errors.experience" class="error">{{ errors.experience }}</span>
+            </div>
+
+            <div class="input-box mb-3">
+              <label class="label">Position</label>
+              <div class="input-wrapper">
+                <select 
+                  class="form-select" 
+                  v-model="formData.position" 
+                  required
+                >
+                  <option value="" disabled>Select Position</option>
+                  <option value="manager">Manager</option>
+                  <option value="facilitator">Facilitator</option>
+                </select>
+              </div>
+              <span v-if="errors.position" class="error">{{ errors.position }}</span>
+            </div>
+
+            <div class="input-box mb-3">
+              <label class="label">Employment Type</label>
+              <div class="input-wrapper">
+                <select 
+                  class="form-select" 
+                  v-model="formData.employmentType" 
+                  required
+                >
+                  <option value="" disabled>Select Employment Type</option>
+                  <option value="full-time">Full-Time</option>
+                  <option value="part-time">Part-Time</option>
+                </select>
+              </div>
+              <span v-if="errors.employmentType" class="error">{{ errors.employmentType }}</span>
+            </div>
+
+            <div class="row mb-3">
+              <div class="col">
+                <div class="input-box">
+                  <label class="label">Qualification</label>
+                  <div class="input-wrapper">
+                    <input 
+                      type="text" 
+                      class="form-control" 
+                      v-model="formData.qualification" 
+                      required
+                    />
+                  </div>
+                  <span v-if="errors.qualification" class="error">{{ errors.qualification }}</span>
+                </div>
+              </div>
+              <div class="col">
+                <div class="input-box">
+                  <label class="label">Date</label>
+                  <div class="input-wrapper">
+                    <input 
+                      type="date" 
+                      class="form-control" 
+                      v-model="formData.date" 
+                      required
+                    />
+                  </div>
+                  <span v-if="errors.date" class="error">{{ errors.date }}</span>
+                </div>
+              </div>
+            </div>
+            <h6>Documentation</h6>
+            <div class="documentation p-3 rounded mb-3">
+              
+              <small class="d-block mb-3">Extension: <span class="text-danger">pdf*docx*jpeg*png</span></small>
+
+              <div class="row">
+                <div class="col-md-6 mb-3">
+                  <div class="input-box">
+                    <label class="label fw-bold">Upload CV</label>
+                    <div class="input-wrapper">
+                      <input 
+                        type="file" 
+                        class="form-control" 
+                        @change="handleFileUpload('cv', $event)"
+                        accept=".pdf,.docx,.jpeg,.png" 
+                        required
+                      />
+                    </div>
+                    <span v-if="errors.cv" class="error">{{ errors.cv }}</span>
+                  </div>
+                </div>
+
+                <div class="col-md-6 mb-3">
+                  <div class="input-box">
+                    <label class="label fw-bold">Upload Highest Degree</label>
+                    <div class="input-wrapper">
+                      <input 
+                        type="file" 
+                        class="form-control" 
+                        @change="handleFileUpload('degree', $event)"
+                        accept=".pdf,.docx,.jpeg,.png" 
+                        required
+                      />
+                    </div>
+                    <span v-if="errors.degree" class="error">{{ errors.degree }}</span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="col-md-6 mb-3">
+                  <div class="input-box">
+                    <label class="label fw-bold">Upload Employment Letter</label>
+                    <div class="input-wrapper">
+                      <input 
+                        type="file" 
+                        class="form-control" 
+                        @change="handleFileUpload('employment', $event)"
+                        accept=".pdf,.docx,.jpeg,.png" 
+                        required
+                      />
+                    </div>
+                    <span v-if="errors.employment" class="error">{{ errors.employment }}</span>
+                  </div>
+                </div>
+
+                <div class="col-md-6 mb-3">
+                  <div class="input-box">
+                    <label class="label fw-bold">Previous Work Experience</label>
+                    <div class="input-wrapper">
+                      <input 
+                        type="file" 
+                        class="form-control" 
+                        @change="handleFileUpload('previousExperience', $event)"
+                        accept=".pdf,.docx,.jpeg,.png" 
+                        required
+                      />
+                    </div>
+                    <span v-if="errors.previousExperience" class="error">{{ errors.previousExperience }}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="small mb-3">
+              <p class="mb-1">i. Professional Work Experience in the relevant Industry will be encouraged</p>
+              <p class="mb-0">ii. Copy of C.V, Highest Degree, Current Employment Letter & Copy of Previous Work experience must be attached</p>
+            </div>
+
+            <button type="submit" class="btn btn-success w-100" :disabled="isSubmitting">
+              {{ isEditing ? 'Update Staff' : 'Add Staff' }}
             </button>
-            <button type="submit" class="btn btn-primary">
-              {{ isEditing ? "Update Staff" : "Add Staff" }}
-            </button>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   </div>
+
+  <!-- Modal backdrop -->
+  <div class="modal-backdrop fade show" v-if="isOpen" @click="closeModal"></div>
 </template>
 
 <script>
@@ -84,11 +213,28 @@ export default {
     return {
       formData: {
         name: "",
-        rank: "",
+        experience: null,
+        position: "",
+        employmentType: "",
+        qualification: "",
+        date: "",
+        cv: null,
+        degree: null,
+        employment: null,
+        previousExperience: null
+      },
+      errors: {
+        name: "",
         experience: "",
         position: "",
+        employmentType: "",
+        qualification: "",
+        date: "",
+        cv: "",
+        degree: "",
+        employment: "",
+        previousExperience: ""
       },
-      ranks: ["1", "2", "3", "4"],
       isSubmitting: false,
     };
   },
@@ -97,50 +243,113 @@ export default {
       this.resetForm();
       this.$emit("close");
     },
+    handleFileUpload(fileType, event) {
+      const file = event.target.files[0];
+      this.formData[fileType] = file;
+      this.errors[fileType] = file ? "" : "File is required";
+    },
+    validateForm() {
+      // Reset errors
+      Object.keys(this.errors).forEach(key => {
+        this.errors[key] = "";
+      });
+
+      // Validate each field
+      let isValid = true;
+
+      if (!this.formData.name) {
+        this.errors.name = "Name is required";
+        isValid = false;
+      }
+
+      if (this.formData.experience === null || this.formData.experience < 0) {
+        this.errors.experience = "Valid experience is required";
+        isValid = false;
+      }
+
+      if (!this.formData.position) {
+        this.errors.position = "Position is required";
+        isValid = false;
+      }
+
+      if (!this.formData.employmentType) {
+        this.errors.employmentType = "Employment type is required";
+        isValid = false;
+      }
+
+      if (!this.formData.qualification) {
+        this.errors.qualification = "Qualification is required";
+        isValid = false;
+      }
+
+      if (!this.formData.date) {
+        this.errors.date = "Date is required";
+        isValid = false;
+      }
+
+      // File validations
+      const fileFields = ['cv', 'degree', 'employment', 'previousExperience'];
+      fileFields.forEach(field => {
+        if (!this.formData[field]) {
+          this.errors[field] = `${field.replace(/([A-Z])/g, ' $1').replace(/^./, str => str.toUpperCase())} is required`;
+          isValid = false;
+        }
+      });
+
+      return isValid;
+    },
     submitForm() {
-      if (!this.validateForm() || this.isSubmitting) {
+      if (this.isSubmitting) return;
+
+      if (!this.validateForm()) {
         return;
       }
 
       this.isSubmitting = true;
 
-      const newStaff = {
+      // Prepare staff data
+      const staffData = {
         name: this.formData.name.trim(),
-        rank: this.formData.rank,
         experience: this.formData.experience,
-        position: this.formData.position.trim(),
+        position: this.formData.position,
+        employmentType: this.formData.employmentType,
+        qualification: this.formData.qualification,
+        date: this.formData.date,
+        files: {
+          cv: this.formData.cv,
+          degree: this.formData.degree,
+          employment: this.formData.employment,
+          previousExperience: this.formData.previousExperience
+        }
       };
 
-      if (this.isValidStaff(newStaff)) {
-        this.$emit("submit", newStaff);
-        this.$emit("close");
-        this.resetForm();
-      }
+      // Emit form data
+      this.$emit('submit', staffData);
+      this.closeModal();
 
       setTimeout(() => {
         this.isSubmitting = false;
       }, 300);
     },
-    isValidStaff(staff) {
-      return (
-        staff.name &&
-        staff.name.length > 0 &&
-        staff.rank &&
-        staff.experience >= 0 &&
-        staff.position &&
-        staff.position.length > 0
-      );
-    },
-    validateForm() {
-      return this.isValidStaff(this.formData);
-    },
     resetForm() {
       this.formData = {
         name: "",
-        rank: "",
-        experience: "",
+        experience: null,
         position: "",
+        employmentType: "",
+        qualification: "",
+        date: "",
+        cv: null,
+        degree: null,
+        employment: null,
+        previousExperience: null
       };
+      
+      // Reset errors
+      Object.keys(this.errors).forEach(key => {
+        this.errors[key] = "";
+      });
+
       this.isSubmitting = false;
     },
   },
@@ -165,119 +374,172 @@ export default {
 </script>
 
 <style scoped>
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.5);
+.input-box {
+  flex: 1;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1050;
+  align-items: stretch;
+  gap: 0;
+  position: relative;
+  border: 1px solid black;
 }
 
-.modal-content {
+.input-box label {
+  color: #333;
+  font-size: 12px;
+  word-wrap: break-word;
+  width: 40%;
+  background-color: #E9FDFC;
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+  margin: 0;
+  border-right: 1px solid black;
+}
+
+.input-wrapper {
+  flex: 1;
+  background: #fff;
+  padding: 0.25rem;
+  border-radius: 0 1px 1px 0;
+  border: 1px solid #e0e0e0;
+  border-left: none;
+  display: flex;
+  align-items: center;
+}
+
+.label {
+  width: 10%;
+}
+
+.input-wrapper input,
+.input-wrapper select {
+  width: 100%;
+  padding: 0.5rem;
+  border: none;
+  background: transparent;
+  outline: none;
+  height: 40px;
+  box-shadow: none;
+}
+
+.input-wrapper input:focus,
+.input-wrapper select:focus {
   background: white;
-  border-radius: 8px;
-  width: 90%;
-  max-width: 500px;
-  max-height: 90vh;
-  overflow-y: auto;
+  border: none;
+  outline: none;
+  box-shadow: none;
+}
+
+.input-box .error {
+  position: absolute;
+  bottom: -1.2rem;
+  left: 0;
+  font-size: 0.8rem;
+  color: #dc3545;
 }
 
 .modal-header {
-  padding: 1rem;
-  border-bottom: 1px solid #dee2e6;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  background-color: #D9D9D9;
 }
 
-.modal-body {
-  padding: 1rem;
-}
-
-.modal-footer {
-  padding: 1rem;
-  border-top: 1px solid #dee2e6;
-  display: flex;
-  justify-content: flex-end;
-  gap: 1rem;
-}
-
-.form-group {
-  margin-bottom: 1rem;
-}
-
-.form-group label {
-  display: block;
-  margin-bottom: 0.5rem;
-  color: #333;
-}
-
-.form-control {
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
-  padding: 0.75rem;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  background-color: white;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
 }
 
-.form-control:focus {
-  border-color: #118e34;
-  outline: none;
-  box-shadow: 0 0 0 2px rgba(17, 142, 52, 0.1);
-}
-
-select.form-control {
-  cursor: pointer;
-  appearance: none;
-  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e");
-  background-repeat: no-repeat;
-  background-position: right 0.75rem center;
-  background-size: 16px 12px;
-  padding-right: 2.5rem;
-}
-
-select.form-control:invalid {
-  color: #6c757d;
-}
-
-select.form-control option {
+input[type="file"]::file-selector-button {
+  background-color: #D9D9D9;
   color: #333;
+  cursor: pointer;
 }
 
-.btn-primary {
-  background: #118e34;
-  color: white;
-  border: none;
+.documentation {
+  background-color: #F7FEFE;
 }
 
-.btn-primary:hover {
-  background: #28a745;
+@media screen and (max-width: 768px) {
+  .modal-dialog {
+    margin: 0;
+    width: 100%;
+    max-width: 100%;
+    height: 100%;
+    max-height: 100%;
+  }
+
+  .modal-content {
+    height: 100%;
+    border-radius: 0;
+  }
+
+  .input-box {
+    flex-direction: column;
+    align-items: stretch;
+    margin-bottom: 1rem;
+  }
+
+  .input-box label {
+    width: 100%;
+    padding: 0.5rem;
+    border-right: none;
+    border-bottom: 1px solid black;
+  }
+
+  .input-wrapper {
+    width: 100%;
+    border-left: 1px solid black;
+    border-top: none;
+  }
+
+  .row {
+    flex-direction: column;
+  }
+
+  .row .col {
+    margin-bottom: 1rem;
+  }
+
+  .modal-body {
+    padding: 1rem;
+    overflow-y: auto;
+    max-height: calc(100vh - 100px);
+  }
+
+  .bg-light {
+    padding: 1rem !important;
+  }
+
+  .small {
+    font-size: 0.7rem;
+  }
 }
 
-.btn-secondary {
-  background: #6c757d;
-  color: white;
-  border: none;
-}
+@media screen and (max-width: 480px) {
+  .input-box label {
+    font-size: 0.8rem;
+  }
 
-.btn-secondary:hover {
-  background: #5a6268;
-}
+  .input-wrapper input,
+  .input-wrapper select {
+    font-size: 0.9rem;
+    padding: 0.3rem;
+  }
 
+  .modal-header {
+    padding: 0.5rem;
+  }
 
-input[type="number"]::-webkit-inner-spin-button,
-input[type="number"]::-webkit-outer-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
+  .modal-title {
+    font-size: 1rem;
+  }
 
-input[type="number"] {
-  -moz-appearance: textfield;
-  appearance: textfield;
+  .btn-success {
+    font-size: 0.9rem;
+    padding: 0.5rem;
+  }
 }
 </style>
